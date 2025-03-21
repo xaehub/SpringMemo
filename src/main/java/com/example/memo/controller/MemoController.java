e8d8a33078dab2c35b5bda08cc2e87ebc2af7f34
xaehub <novxae@gmail.com>
2025-03-21 09:45:31 +0900

    메모 생성 기능(Post)

---
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -0,0 +1,37 @@
+package com.example.memo.controller;
+
+import com.example.memo.dto.MemoRequestDto;
+import com.example.memo.dto.MemoResponseDto;
+import com.example.memo.entity.Memo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/memos")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto) {
+
+        // 식별자(id)가 1씩 증가하도록 만듦
+        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
+
+        // 요청받은 데이터로 Memo 객체 생성
+        Memo memo = new Memo(memoId, dto.getTitle(), dto.getContents());
+
+        // Inmemory DB에 Memo 메모
+        memoList.put(memoId, memo);
+
+        return new MemoResponseDto(memo);
+
+    }
+
+}
---
+++ src/main/java/com/example/memo/entity/Memo.java
@@ -0,0 +1,14 @@
+package com.example.memo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Memo {
+
+    private Long id;
+    private String title;
+    private String contents;
+
+}
