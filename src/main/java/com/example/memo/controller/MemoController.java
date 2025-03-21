0d5b7e0ecbd240bcfa1feb03033c4471a39c63c3
xaehub <novxae@gmail.com>
2025-03-21 09:49:30 +0900

    조회 기능(get)

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -3,10 +3,7 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -18,6 +15,8 @@ public class MemoController {
 
     private final Map<Long, Memo> memoList = new HashMap<>();
 
+
+    // 생성(post)
     @PostMapping
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto) {
 
@@ -32,6 +31,15 @@ public class MemoController {
 
         return new MemoResponseDto(memo);
 
+    }
+
+    // 조회(get)
+    @GetMapping("/{id}")
+    public MemoResponseDto findMemoById(@PathVariable Long id) {
+
+        Memo memo = memoList.get(id);
+
+        return new MemoResponseDto(memo);
     }
 
 }
